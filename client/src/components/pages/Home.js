172846e58e66fa92a6b8dfe7473ca9721036f91d
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-01-24 21:29:25 +0100

    Deleted unecessary atomd/molecules

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,45 +1,41 @@
 import {useContext} from 'react';
 import {context} from "../../context/APIProvider";
-import {nanoid} from "nanoid";
 import H2 from "../atoms/H2";
-import Section from "../molecules/Section";
-import textData from "../../data/textData";
 
 const Home = (props) => {
 
 	const APIContext = useContext(context);
 
-	console.log(props.history)
+	console.log(props.APIContext);
 
 	return (
 		<main>
 			<H2>Home</H2>
 
-			<Section
-				firstname={textData.User.firstname}
-				lastname={textData.User.lastname}
-			/>
-
-			<Section
-				age={textData.User.age}
-				country={textData.User.country}
-			/>
 
 			{APIContext.usersIsFetched
 				? <ol>
-					{APIContext.usersData.data.map((element) => (
-						<li key={nanoid()}>
+					{APIContext.usersData.data.map((user) => (
+						<li key={`usersData${user.id}`}>
 								<span>
-									{`FIRSTNAME: ${element.firstname} - `}
+									{`FIRSTNAME: ${user.firstname}`}
 								</span>
+
 							<span>
-									{`LASTNAME: ${element.lastname}`}
+									{`LASTNAME: ${user.lastname}`}
 								</span>
+
+								<span>
+									{`COUNTRY: ${user.country}`}
+								</span>
+								
+								<span>
+									{`IMAGE_URL: ${user.image_url}`}
+								</span>	
 						</li>
 					))}
 				</ol>
 				: <p>loading...</p>}
-
 		</main>
 	)
 };
