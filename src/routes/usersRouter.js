f3b4ddb3985e089e59d933aeb6d4bb197029244e
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-01-26 21:42:39 +0100

    Added routes but not working yet

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const pool = require('../conf');
 
 
+
+// Read all users
+
 router.get('/', (req, res) => {
 
 	
@@ -62,32 +65,75 @@ router.get('/:id', (req, res) => {
 		// Don't use the connection here, it has been returned to the pool.
 	})
 
-	// //Getting a connection from the pool
-	// pool.getConnection((err, connection) => {
-	// 	// Not connected!
-	// 	if (err) console.error(err);
-	// 	//Using the connection.
-	// 	//Change the query const according to your needs
-	// 	const { id } = req.params;
-	// 	console.log(id);
-	// 	connection.query('SELECT * FROM users WHERE id = ?', [id])
-	// 	.then(([results]) => {
-	// 		if (results.length) {
-	// 		res.json(results[0]);
-	// 		} else {
-	// 		res.sendStatus(404);
-	// 		}
-	// 	});
-	// 		//Realising the connection.
-	// 		connection.release();
-
-	// 		// Handle error after the release.
-	// 		if (err) console.error(err);
-
-	// 		// Don't use the connection here, it has been returned to the pool.
-	// 	})
-	//res.sendStatus(200);
 });
 
+ //Create a user
+ 
+ router.post('/users', (req, res) => {
+ 
+//Getting a connection from the pool
+pool.getConnection((err, connection) => {
+	// Not connected!
+	if (err) console.error(err);
+	//Using the connection.
+	//Change the query const according to your needs
+ 
+
+	//Realising the connection.
+	connection.release();
+
+	// Handle error after the release.
+	if (err) console.error(err);
+
+	// Don't use the connection here, it has been returned to the pool.
+  })
+
+});
+
+//Update a user
+ 
+router.patch('/users', (req, res) => {
+ 
+	//Getting a connection from the pool
+	pool.getConnection((err, connection) => {
+		// Not connected!
+		if (err) console.error(err);
+		//Using the connection.
+		//Change the query const according to your needs
+	 
+	
+		//Realising the connection.
+		connection.release();
+	
+		// Handle error after the release.
+		if (err) console.error(err);
+	
+		// Don't use the connection here, it has been returned to the pool.
+	  })
+	
+	});
+  
+	//Delete a user
+ 
+	router.delete('/users', (req, res) => {
+ 
+		//Getting a connection from the pool
+		pool.getConnection((err, connection) => {
+			// Not connected!
+			if (err) console.error(err);
+			//Using the connection.
+			//Change the query const according to your needs
+		 
+		
+			//Realising the connection.
+			connection.release();
+		
+			// Handle error after the release.
+			if (err) console.error(err);
+		
+			// Don't use the connection here, it has been returned to the pool.
+		  })
+		
+		}); 
 
 module.exports = router;
