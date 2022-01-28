272149ec2fbe0a97281ce9f42c5b3150d01e8e79
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-01-28 22:39:06 +0100

    Added golf course json , map not working yet

--- client/src/components/pages/Map.js
+++ client/src/components/pages/Map.js
@@ -1,24 +1,76 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";  
+import React, { useState ,useEffect } from "react";
+import ReactMapGL,{Marker , Popup} from "react-map-gl"; 
+import * as golfDate from "./data/golf-courses.json";
 
-export default function Map () {
+ 
+export default function App() {
   const [viewport, setViewport] = useState({
-    latitude: 40.7639, 
-    longitude: -73.9794, 
+    latitude: 45.4211,
+    longitude: -75.6903,
     width: "100vw",
     height: "100vh",
     zoom: 10
   });
- 
-    return (
-    
+  const [selectedGolf, setSelectedGolf] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedGolf(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
     <div>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/gc2211/ckyub746v001o16ofu0zy8s1b"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
       >
-       </ReactMapGL> 
-     </div>   
-      );
-  }
+        {golfDate.features.map(golf => (
+          <Marker
+            key={golf.properties.GOLF_ID}
+            latitude={golf.geometry.coordinates[1]}
+            longitude={golf.geometry.coordinates[0]}
+          >
+            <button
+              className="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedGolf(golf);
+              }}
+            >
+              <img src="/golf.svg" alt="Golf Course Icon" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedGolf ? (
+          <Popup
+            latitude={selectedGolf.geometry.coordinates[1]}
+            longitude={selectedGolf.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedGolf(null);
+            }}
+          >
+            <div>
+              <h2>{selectedGolf.properties.ID}</h2>
+              <p>{selectedGolf.properties.LABEL}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
+  );
+} 
  
+   
