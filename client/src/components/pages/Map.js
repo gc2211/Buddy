48a28ffe01703f36439a1dd294e74a4e28377b7f
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-01-29 23:11:50 +0100

    Test

--- client/src/components/pages/Map.js
+++ client/src/components/pages/Map.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect } from "react";
 import ReactMapGL,{Marker , Popup} from "react-map-gl"; 
-import * as golfDate from "./data/golf-courses.json";
+//import * as golfDate from "./golf-courses.js";
 
  
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
     height: "100vh",
     zoom: 10
   });
-  const [selectedGolf, setSelectedGolf] = useState(null);
+ /* const [selectedGolf, setSelectedGolf] = useState(null);
 
   useEffect(() => {
     const listener = e => {
@@ -24,50 +24,16 @@ export default function App() {
     return () => {
       window.removeEventListener("keydown", listener);
     };
-  }, []);
+  }, []);*/
 
   return (
     <div>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/gc2211/ckyub746v001o16ofu0zy8s1b"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
+       // mapStyle="mapbox://styles/gc2211/ckyub746v001o16ofu0zy8s1b"
       >
-        {golfDate.features.map(golf => (
-          <Marker
-            key={golf.properties.GOLF_ID}
-            latitude={golf.geometry.coordinates[1]}
-            longitude={golf.geometry.coordinates[0]}
-          >
-            <button
-              className="marker-btn"
-              onClick={e => {
-                e.preventDefault();
-                setSelectedGolf(golf);
-              }}
-            >
-              <img src="/golf.svg" alt="Golf Course Icon" />
-            </button>
-          </Marker>
-        ))}
-
-        {selectedGolf ? (
-          <Popup
-            latitude={selectedGolf.geometry.coordinates[1]}
-            longitude={selectedGolf.geometry.coordinates[0]}
-            onClose={() => {
-              setSelectedGolf(null);
-            }}
-          >
-            <div>
-              <h2>{selectedGolf.properties.ID}</h2>
-              <p>{selectedGolf.properties.LABEL}</p>
-            </div>
-          </Popup>
-        ) : null}
+        hello world
       </ReactMapGL>
     </div>
   );
