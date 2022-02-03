64a9737e5008832b4f143113d1c82f0beec58fd7
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-02-03 22:41:12 +0100

    Map is working on Google Chrome

--- client/src/components/pages/Map.js
+++ client/src/components/pages/Map.js
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 export default function App() {
   const [viewport, setViewport] = useState({
-    latitude: 45.4211,
-    longitude: -75.6903,
+    latitude: 40.730610,
+    longitude: -73.935242,
     width: "100vw",
     height: "100vh",
-    zoom: 10
+    zoom: 8
   });
   return (
     <div>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken="pk.eyJ1IjoiZ2MyMjExIiwiYSI6ImNreXcxc2s2aTA0YjEybm14Njc1M2JwMzQifQ.VqbFEjTIiNaweT1lK7mU6w"
-        mapStyle="mapbox://styles/gc2211/ckz0gd9uf004r14nt02brjaq8"
+        mapStyle="mapbox://styles/gc2211/ckz7h3bzy005q14s5i76uhvre"
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
       >
+      <button>Events</button>
       </ReactMapGL>
     </div>
   );
